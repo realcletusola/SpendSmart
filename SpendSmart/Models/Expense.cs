555587c4acd272a38eb325011f01922046c20673
cletus <cletushunsu@gmail.com>
2024-08-13 03:03:43 -0700

    created expense model

--- SpendSmart/Models/Expense.cs
+++ SpendSmart/Models/Expense.cs
@@ -1,9 +1,15 @@
-﻿namespace SpendSmart.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
 {
     public class Expense
     {
         public int Id { get; set; }
+
         public decimal Value { get; set; }
-        public string Description { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
     }
 }
