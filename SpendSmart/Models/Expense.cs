cce33e8fa60e398ed7b12c0cfc60e59b3447fbd0
ORION <ORION@BISHOP>
2024-08-13 02:59:23 -0700

    Add project files.

---
+++ SpendSmart/Models/Expense.cs
@@ -0,0 +1,9 @@
+﻿namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public string Description { get; set; }
+    }
+}
