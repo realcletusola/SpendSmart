7dbaaccd66e1f720b575ad155111c80ddf6082f4
cletus <cletushunsu@gmail.com>
2024-08-13 04:26:42 -0700

    updated code to edit and delete expenses

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -8,8 +8,10 @@ namespace SpendSmart.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        // Db context for SpendSmart
         private readonly SpendSmartDbContext _context;
 
+        // Include Db context to Home controller
         public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
         {
             _logger = logger;
@@ -20,22 +22,65 @@ namespace SpendSmart.Controllers
         {
             return View();
         }
-
+        
+        // expense controller
         public IActionResult Expenses()
         {
+            // get all expense as list 
+            var allExpenses = _context.Expenses.ToList();
+
+            // return all expense
+            return View(allExpenses);
+        }
+
+        // create/edit expense controller
+        public IActionResult CreateEditExpense(int? id)
+        {
+            // check if id is not null
+            if (id == null)
+            {
+                // get expense by id
+                var expenseInDb = _context.Expenses.SingleOrDefault(e => e.Id == id);
+                // return expense 
+                return View(expenseInDb);
+            }
+
+            // return view if id is null
             return View();
         }
 
-        public IActionResult CreateEditExpense()
+        // delete expense controller
+        public IActionResult Delete(int id)
         {
-            return View();
-        }
-
-        public IActionResult CreateEditExpenseForm(Expense model)
-        {
-            _context.Expenses.Add(model);
+            // get expense
+            var expenseInDb = _context.Expenses.SingleOrDefault(e => e.Id == id);
+            // remove expense 
+            _context.Expenses.Remove(expenseInDb);
+            // save changes to database 
             _context.SaveChanges();
 
+            // redirect to expense page
+            return RedirectToAction("Expenses");
+        }
+
+        // expense submit form controller
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+
+            if (model.Id == 0)
+            {
+                //create 
+                _context.Expenses.Add(model);
+            }
+            else
+            {
+                // edit 
+                _context.Expenses.Update(model);
+            }
+                // save changes to database 
+            _context.SaveChanges();
+
+            // redirect to expense page
             return RedirectToAction("Expenses");
         }
         public IActionResult Privacy()
